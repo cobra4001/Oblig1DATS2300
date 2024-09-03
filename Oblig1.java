5c3ff287e62bb341ef08f70eaa8275c7de8507e5
cobra4001 <jnormvalle@gmail.com>
2024-09-03 14:08:13 +0200

    Gjort oppgave 0

--- Oblig1.java
+++ Oblig1.java
@@ -1,1 +1,24 @@
+public class Oblig1 {
 
+
+    public static void main(String [] args){
+
+        System.out.println("Antall personer i gruppen er " +gruppeMedlemmer());
+
+    }
+
+
+    // Oppgave 0
+    //Denne koden returnerer antall medlemmer i gruppen
+    public static int gruppeMedlemmer(){
+
+        return 3;
+    }
+
+
+
+
+
+
+
+}
